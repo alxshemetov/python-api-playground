bb8a70c5a58e25a9b7847b542a0f67437d75eb01
aleksei.shemetov <alxshemetov@gmail.com>
2025-10-19 15:59:19 +0200

    Add ArtistsService to encapsulate API interactions for artist resources

---
+++ src/python_api_playground/services/artists_service.py
@@ -0,0 +1,42 @@
+from typing import List
+
+from python_api_playground.api_client import APIClient
+from python_api_playground.models.artists_model import (
+    ArtistCreate,
+    ArtistResponse,
+    ArtistUpdate,
+)
+
+
+class ArtistsService:
+
+    def __init__(self, client: APIClient):
+        self.client = client
+        self.endpoint = "/artists"
+
+    def get_all_artists(self) -> List[ArtistResponse]:
+        response = self.client.get(self.endpoint)
+        response.raise_for_status()
+        return [ArtistResponse.model_validate(artist) for artist in response.json()]
+
+    def get_artist_by_id(self, user_id: str) -> ArtistResponse:
+        response = self.client.get(f"{self.endpoint}/{user_id}")
+        response.raise_for_status()
+        return ArtistResponse.model_validate(response.json())
+
+    def create_artist(self, new_artist: ArtistCreate):
+        payload = new_artist.model_dump()
+        response = self.client.post(self.endpoint, json=payload)
+        response.raise_for_status()
+        return response
+
+    def update_artist(self, updated_artist: ArtistUpdate):
+        payload = updated_artist.model_dump()
+        response = self.client.put(self.endpoint, json=payload)
+        response.raise_for_status()
+        return response
+
+    def delete_artist(self, user_id: str):
+        response = self.client.delete(f"{self.endpoint}/{user_id}")
+        response.raise_for_status()
+        return response
