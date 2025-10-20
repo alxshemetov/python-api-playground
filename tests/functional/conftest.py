79610a24b90f333debd47f295eb19cd1de404f1a
aleksei.shemetov <alxshemetov@gmail.com>
2025-10-20 23:44:05 +0200

    Refactor functional tests to replace `artist_steps` with `create_new_artist` fixture and remove redundant ArtistSteps class.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,9 +1,10 @@
+import http
+
 import pytest
 from faker import Faker
 
 from python_api_playground.models.artists_model import ArtistCreate
 from python_api_playground.services.artists_service import ArtistsService
-from tests.functional.steps.artists_steps import ArtistSteps
 
 fake = Faker()
 
@@ -13,11 +14,6 @@ def artists_service(api_client):
     return ArtistsService(api_client)
 
 
-@pytest.fixture(scope="module")
-def artist_steps(artists_service):
-    return ArtistSteps(artists_service)
-
-
 @pytest.fixture
 def generate_artist_data() -> ArtistCreate:
     return ArtistCreate(
@@ -25,3 +21,18 @@ def generate_artist_data() -> ArtistCreate:
         last_name=fake.last_name(),
         birth_year=fake.year()
     )
+
+
+@pytest.fixture
+def create_new_artist(artists_service, generate_artist_data):
+    new_artist = generate_artist_data
+    create_response = artists_service.create_artist(new_artist)
+    assert create_response.status_code == http.HTTPStatus.OK
+    user_id = create_response.json()
+
+    yield user_id, new_artist
+
+    try:
+        artists_service.delete_artist(str(user_id))
+    except Exception:
+        pass
--- tests/functional/test_artists_api_functional.py
+++ tests/functional/test_artists_api_functional.py
@@ -8,9 +8,8 @@ from tests.functional.helpers.artists_helpers import assert_artist_data
 from tests.functional.test_data import artist_test_payloads as payloads
 
 
-def test_create_artist(artists_service, artist_steps, generate_artist_data):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_create_artist(artists_service, create_new_artist):
+    user_id, new_artist = create_new_artist
 
     all_artists_response = artists_service.get_all_artists()
     assert any(artist.user_id == user_id for artist in all_artists_response)
@@ -19,17 +18,14 @@ def test_create_artist(artists_service, artist_steps, generate_artist_data):
     assert_artist_data(artist_response, user_id, new_artist)
 
 
-def test_get_all_artists(artists_service, artist_steps, generate_artist_data):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_get_all_artists(artists_service, create_new_artist):
+    user_id, new_artist = create_new_artist
 
     all_artists = artists_service.get_all_artists()
-
     assert isinstance(all_artists, list)
     assert len(all_artists) > 0, "The artists list should not be empty."
 
     found_artist = next((artist for artist in all_artists if artist.user_id == user_id), None)
-
     assert found_artist is not None, "The created artist was not found in the GET /artists list."
     assert isinstance(found_artist, ArtistResponse)
     assert found_artist.first_name == new_artist.first_name
@@ -37,17 +33,15 @@ def test_get_all_artists(artists_service, artist_steps, generate_artist_data):
     assert str(found_artist.birth_year) == new_artist.birth_year
 
 
-def test_get_artist_by_id(artists_service, artist_steps, generate_artist_data):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_get_artist_by_id(artists_service, create_new_artist):
+    user_id, new_artist = create_new_artist
 
     artist_response: ArtistResponse = artists_service.get_artist_by_id(str(user_id))
     assert_artist_data(artist_response, user_id, new_artist)
 
 
-def test_update_artist(artists_service, artist_steps, generate_artist_data):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_update_artist(artists_service, create_new_artist):
+    user_id, new_artist = create_new_artist
 
     update_data = ArtistUpdate(
         user_id=str(user_id),
@@ -64,9 +58,8 @@ def test_update_artist(artists_service, artist_steps, generate_artist_data):
     assert_artist_data(updated_artist_response, user_id, update_data)
 
 
-def test_delete_artist(artists_service, artist_steps, generate_artist_data):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_delete_artist(artists_service, create_new_artist):
+    user_id, new_artist = create_new_artist
 
     delete_response = artists_service.delete_artist(str(user_id))
     assert delete_response.status_code == HTTPStatus.OK
@@ -81,7 +74,6 @@ def test_create_artist_with_empty_field(artists_service, artist_payload):
     invalid_artist = ArtistCreate(**artist_payload)
 
     error_response = artists_service.create_artist(invalid_artist)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "All fields must be non-empty strings")
 
 
@@ -90,14 +82,12 @@ def test_create_artist_with_missing_field(artists_service, artist_payload):
     invalid_artist = ArtistCreate(**artist_payload)
 
     error_response = artists_service.create_artist(invalid_artist)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "Missing keys")
 
 
 @pytest.mark.parametrize("artist_payload", payloads.CREATE_ARTIST_INVALID_DATA_TYPE_PAYLOADS)
 def test_create_artist_with_invalid_data_type(artists_service, artist_payload):
     error_response = artists_service.client.post("/artists", json=artist_payload)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "All fields must be non-empty strings")
 
 
@@ -105,38 +95,32 @@ def test_create_artist_with_empty_payload(artists_service):
     invalid_artist = ArtistCreate()
 
     error_response = artists_service.create_artist(invalid_artist)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "Invalid JSON payload")
 
 
 def test_get_non_existent_artist(artists_service):
     error_response = artists_service.get_artist_by_id("non-existent-id")
-
     assert error_response.status_code == HTTPStatus.NOT_FOUND
 
 
 def test_get_artist_with_empty_id(artists_service):
     error_response = artists_service.get_artist_by_id("")
-
     assert error_response.status_code == HTTPStatus.NOT_FOUND
 
 
 @pytest.mark.parametrize("update_payload", payloads.UPDATE_ARTIST_EMPTY_FIELD_PAYLOADS)
-def test_update_artist_with_empty_field(artists_service, artist_steps, generate_artist_data, update_payload):
-    new_artist = generate_artist_data
-    artist_steps.create_artist(new_artist)
+def test_update_artist_with_empty_field(artists_service, create_new_artist, update_payload):
+    user_id, new_artist = create_new_artist
 
     invalid_update = ArtistUpdate(**update_payload)
 
     error_response = artists_service.update_artist(invalid_update)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "All fields must be non-empty strings")
 
 
 @pytest.mark.parametrize("update_payload", payloads.UPDATE_ARTIST_MISSING_FIELD_PAYLOADS)
-def test_update_artist_with_missing_field(artists_service, artist_steps, generate_artist_data, update_payload):
-    new_artist = generate_artist_data
-    user_id = artist_steps.create_artist(new_artist)
+def test_update_artist_with_missing_field(artists_service, create_new_artist, update_payload):
+    user_id, new_artist = create_new_artist
 
     if "user_id" in update_payload:
         update_payload["user_id"] = str(user_id)
@@ -144,11 +128,9 @@ def test_update_artist_with_missing_field(artists_service, artist_steps, generat
     invalid_update = ArtistUpdate(**update_payload)
 
     error_response = artists_service.update_artist(invalid_update)
-
     assert_error_response(error_response, HTTPStatus.BAD_REQUEST, "Missing keys")
 
 
 def test_delete_artist_with_invalid_id(artists_service, artist_steps):
     error_response = artists_service.delete_artist("non-existent-id")
-
     assert error_response.status_code == HTTPStatus.NOT_FOUND
